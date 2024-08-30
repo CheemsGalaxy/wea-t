8e74ca3e57b67c9cb4d5033f6854d21cd582d508
CheemsGalaxy <149580222+CheemsGalaxy@users.noreply.github.com>
2024-08-30 09:07:12 +0700

    Add files via upload

---
+++ Wea-T-Page/assets/javascript/weather.js
@@ -0,0 +1,45 @@
+var search = document.querySelector('#search_box');
+var locationText = document.querySelector('.location-text');
+var tempText = document.querySelector('.temp-text');
+var weatherStatus = document.querySelector('.weather-status');
+var utcTime = document.querySelector('.info-time');
+var windSpeed = document.querySelector('.info-wind-speed');
+var humidity = document.querySelector('.info-humidity');
+var visibility = document.querySelector('.info-visibility');
+
+async function changWeatherUI() {
+  let capitalSearch = search.value.trim();
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=6a8fc30989f23512670b83cbdf2f238f`
+
+  let data  = await fetch(apiURL).then(res => res.json())
+  if(data.cod = 200) {
+    console.log('data weather', data);
+    locationText.innerText = data.name + ', ' + data.sys.country;
+    tempText.innerText = (data.main.temp - 273.15).toFixed(1) + ' °C';
+    weatherStatus.innerText = data.weather[0].description;
+    windSpeed.innerText = '💨 Gió : ' + data.wind.speed + ' m/s'
+    humidity.innerText = '💧 Độ ẩm: ' + data.main.humidity  + '%'
+    visibility.innerText = '👁 Tầm nhìn: ' + data.visibility + 'm'
+  } else {
+  
+  }
+}
+
+changWeatherUI()
+
+async function dateUI() {
+  const timeUTC = new Date();
+  const day =  timeUTC.getDate();
+  const month = timeUTC.getMonth() + 1;
+  const year = timeUTC.getFullYear();
+  var realTime = (`${day} / ${month} / ${year}`);
+  utcTime.innerText = realTime;
+}
+
+dateUI()
+
+search.addEventListener('keypress', function(e){
+  if(e.code === 'Enter') {
+    changWeatherUI()
+  }
+})
